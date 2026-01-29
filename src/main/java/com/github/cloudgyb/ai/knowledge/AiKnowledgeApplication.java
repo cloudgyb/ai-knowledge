4302fa1ff63c58dada82a694d99867eef08aad0d
cloudgyb <gyb_cloud@163.com>
2026-01-29 22:07:57 +0800

    feat(project): 初始化AI知识库项目基础架构

---
+++ src/main/java/com/github/cloudgyb/ai/knowledge/AiKnowledgeApplication.java
@@ -0,0 +1,13 @@
+package com.github.cloudgyb.ai.knowledge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiKnowledgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiKnowledgeApplication.class, args);
+    }
+
+}
