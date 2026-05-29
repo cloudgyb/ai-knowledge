2ac74b07658e4fec1ece78480b20f75bff55c348
cloudgyb <gyb_cloud@163.com>
2026-05-29 17:20:03 +0800

    refactor(config): 重构PGVector配置类并移除RAG配置
    - 移除独立的RAGConfiguration配置类及其相关bean定义

--- knowledge-base-server/src/main/java/com/github/cloudgyb/ai/knowledge/server/config/PgVectorEmbeddingStorageProperties.java
+++ knowledge-base-server/src/main/java/com/github/cloudgyb/ai/knowledge/server/config/PgVectorEmbeddingStorageProperties.java
@@ -1,15 +1,22 @@
 package com.github.cloudgyb.ai.knowledge.server.config;
 
 import dev.langchain4j.store.embedding.pgvector.MetadataStorageMode;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 /**
+ * PGVector 向量存储配置
  *
  * @author cloudgyb
  * @since 2026/3/3 15:32
  */
+@Setter
+@Getter
 @ConfigurationProperties(prefix = "ai.embedding.storage.pgvector")
-public final class PgVectorEmbeddingStorageProperties {
+@Configuration
+public class PgVectorEmbeddingStorageProperties {
     private String host;
     private Integer port;
     private String database;
@@ -18,59 +25,4 @@ public final class PgVectorEmbeddingStorageProperties {
     private String table;
     private MetadataStorageMode metadataStorageMode;
 
-    public String getHost() {
-        return host;
-    }
-
-    public void setHost(String host) {
-        this.host = host;
-    }
-
-    public Integer getPort() {
-        return port;
-    }
-
-    public void setPort(Integer port) {
-        this.port = port;
-    }
-
-    public String getDatabase() {
-        return database;
-    }
-
-    public void setDatabase(String database) {
-        this.database = database;
-    }
-
-    public String getUser() {
-        return user;
-    }
-
-    public void setUser(String user) {
-        this.user = user;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getTable() {
-        return table;
-    }
-
-    public void setTable(String table) {
-        this.table = table;
-    }
-
-    public MetadataStorageMode getMetadataStorageMode() {
-        return metadataStorageMode;
-    }
-
-    public void setMetadataStorageMode(MetadataStorageMode metadataStorageMode) {
-        this.metadataStorageMode = metadataStorageMode;
-    }
 }
