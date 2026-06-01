755e4936043a82f28e1140870802d3baf4674800
cloudgyb <gyb_cloud@163.com>
2026-06-01 17:11:16 +0800

    feat(config): 添加HTTP调试配置和自定义OkHttpClient构建器
    - 在application.yaml和application-home.yaml中添加http.debug配置选项
    - 创建OkHttpClientCustomBuilderFactory类实现HTTP请求响应调试功能
    - 添加Spring ApplicationContext集成以动态控制调试开关

--- knowledge-base-server/src/main/java/com/github/cloudgyb/ai/knowledge/server/config/OkHttpClientCustomBuilderFactory.java
+++ knowledge-base-server/src/main/java/com/github/cloudgyb/ai/knowledge/server/config/OkHttpClientCustomBuilderFactory.java
@@ -3,23 +3,50 @@ package com.github.cloudgyb.ai.knowledge.server.config;
 import dev.langchain4j.http.client.HttpClientBuilderFactory;
 import dev.langchain4j.http.client.okhttp.OkHttpClientBuilder;
 import dev.langchain4j.http.client.okhttp.OkHttpClientBuilderFactory;
+import lombok.extern.slf4j.Slf4j;
 import okhttp3.logging.HttpLoggingInterceptor;
+import org.jspecify.annotations.NonNull;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
 
 /**
- * 自定义 OkHttpClientBuilderFactory
+ * 自定义 OkHttpClientBuilderFactory，
+ * 必须使用以下 JVM 参数：
+ * <pre>
+ * -Dlangchain4j.http.clientBuilderFactory=com.github.cloudgyb.ai.knowledge.server.config.OkHttpClientCustomBuilderFactory
+ * </pre>
  *
  * @author cloudgyb
  * @since 2026/5/28 13:40
  */
-public class OkHttpClientCustomBuilderFactory extends OkHttpClientBuilderFactory implements HttpClientBuilderFactory {
+@Slf4j
+@Component
+public class OkHttpClientCustomBuilderFactory extends OkHttpClientBuilderFactory implements HttpClientBuilderFactory,
+        ApplicationContextAware {
+    private static volatile boolean debug = true;
+
     @Override
     public OkHttpClientBuilder create() {
         OkHttpClientBuilder okHttpClientBuilder = super.create();
+        if (!debug) {
+            return okHttpClientBuilder;
+        }
+        if (log.isInfoEnabled()) {
+            log.info("启用 okhttp debug,将输出请求和响应详情");
+        }
         okhttp3.OkHttpClient.Builder nativeOkHttpClientBuilder = new okhttp3.OkHttpClient.Builder();
         HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
         loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
         nativeOkHttpClientBuilder.addNetworkInterceptor(loggingInterceptor);
         okHttpClientBuilder.okHttpClientBuilder(nativeOkHttpClientBuilder);
         return okHttpClientBuilder;
+    }
+
+    @Override
+    public void setApplicationContext(@NonNull ApplicationContext applicationContext) throws BeansException {
+        Boolean property = applicationContext.getEnvironment().getProperty("ai.http.debug", boolean.class);
+        debug = property != null && property;
     }
 }
